6114accd9de36a20ec6349b8a5eb11481906a7b3
AbrarNitk <abrar.nitk@gmail.com>
2020-04-11 14:16:50 +0530

    defined secret key config

--- encrypted_id/src/lib.rs
+++ encrypted_id/src/lib.rs
@@ -1,3 +1,29 @@
+#[macro_use]
+extern crate lazy_static;
+
+use failure::_core::marker::PhantomData;
+
+pub type EResult<T> = std::result::Result<T, failure::Error>;
+
+mod encrypt;
+mod decrypt;
+
+#[derive(Default)]
+pub struct Config {
+    secret_key: Option<String>,
+    secret_key_bytes: Vec<u8>,
+}
+
+lazy_static! {
+    pub(crate) static ref CONFIG: std::sync::RwLock<Config> = std::sync::RwLock::new(Config::default());
+}
+
+pub fn init_conf(secret_key: &str) {
+    let mut conf = CONFIG.write().unwrap();
+    conf.secret_key = Some(secret_key.to_string());
+    conf.secret_key_bytes = secret_key.as_bytes().to_owned();
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
